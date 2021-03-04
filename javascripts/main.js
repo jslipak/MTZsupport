ba7e092fbdeb913a38f90f0c6bdb29daae506988
jslipak <seriedigital@gmail.com>
2021-03-04 16:18:39 -0300

    entrega array clase 6

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,21 +1,23 @@
 class FormMsg {
-    constructor(fullName, email, telephone, address, msgInput){
+    constructor(fullName, email, telephone, address, servicios, msgInput){
         this.fullName = fullName;
         this.email = email;
         this.telephone = telephone;
         this.address = address;
+        this.servicios = servicios
         this.msgInput = msgInput;
     }
-    
     printAllInputs = function(){
-        console.log(`El nombre completo es: ${this.fullName} \n Email: ${this.email} \n Teléfono: ${this.telephone} \n Dirección: ${this.address} \n Consulta: ${this.msgInput}`);
+        console.log(`El nombre completo es: ${this.fullName} \n Email: ${this.email} \n Teléfono: ${this.telephone} \n Dirección: ${this.address} \n Los Servicios son: ${this.servicios.join()}\n Consulta: ${this.msgInput}`);
     }
+
 }
 
 function inputString(str) {
     return prompt("Ingrese su " + str +": ");
 }
 
-msg = new FormMsg(inputString("Nombre Completo"), inputString("email"), inputString("teléfono"), inputString("Dirección"), inputString("mensaje"));
+msg = new FormMsg(inputString("Nombre Completo"), inputString("email"), inputString("teléfono"), inputString("Dirección"),["Visita Domicilio", "instalar Gadget"], inputString("mensaje"));
 
-msg.printAllInputs();
+msg.printAllInputs()
+console.log(msg.fullName.toUpperCase())
