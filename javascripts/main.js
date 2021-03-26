8b6e6cc9470079e39f548233fab9041e8d9aae45
jslipak <seriedigital@gmail.com>
2021-03-26 10:10:45 -0300

    Revert "entrega proyecto segunda revisión"
    This reverts commit 61e03add474ed4b53d5d3863f8b6896bc7d2b8f5.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,65 +1,46 @@
-//Clases
+//Clases 
 class FormMsg {
-  constructor(fullName, email, telephone, address, servicios, msgInput) {
-    this.fullName = fullName;
-    this.email = email;
-    this.telephone = telephone;
-    this.address = address;
-    this.servicios = servicios;
-    this.msgInput = msgInput;
-  }
-  printAllInputs = function () {
-    console.log(
-      `El nombre completo es: ${this.fullName} \n Email: ${
-        this.email
-      } \n Teléfono: ${this.telephone} \n Dirección: ${
-        this.address
-      } \n Los Servicios son: ${this.servicios.join()}\n Consulta: ${
-        this.msgInput
-      }`,
-    );
-  };
+    constructor(fullName, email, telephone, address, servicios, msgInput) {
+        this.fullName = fullName;
+        this.email = email;
+        this.telephone = telephone;
+        this.address = address;
+        this.servicios = servicios;
+        this.msgInput = msgInput;
+    }
+    printAllInputs = function() {
+        console.log(`El nombre completo es: ${this.fullName} \n Email: ${this.email} \n Teléfono: ${this.telephone} \n Dirección: ${this.address} \n Los Servicios son: ${this.servicios.join()}\n Consulta: ${this.msgInput}`);
+    }
+
 }
 
 //Funciones
 
 function inputString(str) {
-  return prompt('Ingrese su ' + str + ': ');
+    return prompt("Ingrese su " + str + ": ");
 }
 
 function isEmail(email) {
-  RegExpEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-  return RegExpEmail.test(email);
+    RegExpEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return RegExpEmail.test(email)
 }
-//Selectores
-let name = document.querySelector(".form-group input[name='name']");
+//Selectores 
 let email = document.querySelector(".form-group input[name='email']");
-let phone = document.querySelector(".form-group input[name='phone']");
-let address = document.querySelector(".form-group input[name='address']");
-let msg = document.getElementById('msgID');
-let input = document.getElementsByTagName('input');
-let checkbox = document.getElementsByClassName('form-check-input');
+let msg = document.getElementById("msgID");
+let input = document.getElementsByTagName("input");
+let checkbox = document.getElementsByClassName("form-check-input");
 let btnModal = document.querySelector('button.btn.btn-success.float-right');
 
-window.onload = function () {
-  email.addEventListener('input', function (e) {
-    console.log(e);
-    if (isEmail(e.target.value)) {
-      email.classList.remove('wrong');
-      btnModal.disabled = false;
-    } else {
-      email.classList.add('wrong');
-      btnModal.disabled = true;
-    }
-  });
-  btnModal.addEventListener('click', function (e) {
-    let modalSubmit = new FormMsg(
-      name.value,
-      email.value,
-      phone.value,
-      address.value,
-      msg.value,
-    );
-    localStorage.setItem('lastMSG', JSON.stringify(modalSubmit));
-  });
-};
+
+window.onload = function() { 
+    email.addEventListener("input",function (e) {
+        console.log(e)
+        if (isEmail(e.target.value)) {
+            email.classList.remove("wrong")
+            btnModal.disabled = false;
+        } else {
+            email.classList.add("wrong")
+            btnModal.disabled = true;
+        }
+    })
+}
