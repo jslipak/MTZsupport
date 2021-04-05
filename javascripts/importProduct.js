16763cf327f9ee730e3a410a72c52719781ff722
jslipak <seriedigital@gmail.com>
2021-04-05 09:42:27 -0300

    fix fetch

--- javascripts/importProduct.js
+++ javascripts/importProduct.js
@@ -1,4 +1,12 @@
+url =
+  'https://github.com/jslipak/jslipak.github.io/tree/main/data/product.json';
 let product = [];
-fetch('/data/product.json')
+fetch(url)
   .then((res) => res.json())
   .then((data) => (product = data));
+
+//$.getJSON('/data/product.json', function (json) {
+//product = json;
+//});
+
+console.log(product);
