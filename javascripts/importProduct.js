f6e2f2564ef3f71cdf52b81896bb7bfe968b312a
jslipak <seriedigital@gmail.com>
2021-04-05 18:29:02 -0300

    solve fix fetch

--- javascripts/importProduct.js
+++ javascripts/importProduct.js
@@ -1,14 +1,30 @@
 const URL = `https://jslipak.github.io/data/product.json`;
 let product = [];
-$.ajax({
-  method: 'GET',
-  url: URL,
-})
-  .done(function (data) {
+fetch(URL)
+  .then((res) => res.json())
+  .then((data) => {
     product = data;
-  })
-  .fail(function (error) {
-    error = alert('no se establecio la conexion');
+    if (typeof product !== 'undefined') {
+      string_to_add = '';
+      product.forEach((e) => {
+        if ((e.todo == '') & (e.note == '')) {
+          string_to_add += `
+  <div class="col-md-4 d-flex align-items-strech">
+  <div class="card mb-4 cardColorBG">
+  <img class="card-img-top" src="${e.imagen}" alt="${e.imagen}">
+  <div class="card-body d-flex flex-column">
+  <h5 class="card-title">${e.title}</h5>
+  <p class="card-text">${e.description}</p>
+  <button type="button" class="btn btn-outline-light" data-toggle="modal" data-target="#ModalForm">Mas información</button>
+  </div>
+  </div>
+  </div>
+  `;
+        }
+      });
+      //$('#short_cards').append(string_to_add);
+      document
+        .querySelector('#short_cards')
+        .insertAdjacentHTML('beforeend', string_to_add);
+    }
   });
-
-console.log(product);
